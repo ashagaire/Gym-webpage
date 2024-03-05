6ba49ae923f0e9dc3218ead2bfdc402476459cc4
ashagaire <asha.gaire95@gmail.com>
2024-03-05 17:21:25 +0200

    first commit, hero, product and review sections done

---
+++ src/components/Header.svelte
@@ -0,0 +1,24 @@
+<script>
+
+</script>
+<header class="flex flex-col relative z-20">
+    <div class="max-w-[1400px] mx-auto w-full flex items-center justify-between p-4 py-6">
+        <a href="/">
+            <h1 class="font-semibold">
+                Swoley <span class="text-indigo-400">Moley</span>
+            </h1>
+        </a>
+        <button class="md:hidden grid place-item-center">
+            <i class="fa-solid fa-bars"></i>
+        </button>
+        <nav class="hidden md:flex item-center gap-4 lg:gap-6">
+            <a class="duration-200 hover:text-indigo-400 cursor-pointer" href="#products" > Products</a>
+            <a class="duration-200 hover:text-indigo-400 cursor-pointer" href="#review">Reviews</a>
+            <a class="duration-200 hover:text-indigo-400 cursor-pointer" href="#faqs">FAQs</a>
+        </nav>
+        <button class="specialBtn">
+            <p>Start free today </p>
+        </button>
+    </div>
+
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    import "../app.css";
+    import Footer from "../components/Footer.svelte";
+  </script>
+  
+  <slot />
+  <Footer />
+
+  <!-- hedader
+    hero
+    product description
+    user reviews
+    faq
+    conversion-
+    footer
+    -->
