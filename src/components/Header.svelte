c41c54ccada50dd29f677293948fc350c9577ccf
ashagaire <asha.gaire95@gmail.com>
2024-03-06 11:10:44 +0200

    Frontend update

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,5 @@
 <script>
-
+    import{openModal} from "../store"
 </script>
 <header class="flex flex-col relative z-20">
     <div class="max-w-[1400px] mx-auto w-full flex items-center justify-between p-4 py-6">
@@ -8,17 +8,18 @@
                 Swoley <span class="text-indigo-400">Moley</span>
             </h1>
         </a>
-        <button class="md:hidden grid place-item-center">
+        <button on:click={() => ($openModal= true)} class="md:hidden grid place-item-center" >
             <i class="fa-solid fa-bars"></i>
         </button>
         <nav class="hidden md:flex item-center gap-4 lg:gap-6">
-            <a class="duration-200 hover:text-indigo-400 cursor-pointer" href="#products" > Products</a>
+            <a class="duration-200 hover:text-indigo-400 cursor-pointer" href="#product" > Products</a>
             <a class="duration-200 hover:text-indigo-400 cursor-pointer" href="#review">Reviews</a>
             <a class="duration-200 hover:text-indigo-400 cursor-pointer" href="#faqs">FAQs</a>
-        </nav>
+        
         <button class="specialBtn">
             <p>Start free today </p>
         </button>
+    </nav>
     </div>
 
 </header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,15 @@
     import Hero from "../components/Hero.svelte";
     import Product from "../components/Product.svelte";
     import Reviews from "../components/Reviews.svelte";
+    import FAQs from "../components/FAQs.svelte";
+    import Conversion from "../components/Conversion.svelte";
+    import Footer from "../components/Footer.svelte";
 </script>
 <main class="flex flex-col">
     <Hero />
     <Product />
     <Reviews />
+    <FAQs />
+    <Conversion />
+    
 </main>
