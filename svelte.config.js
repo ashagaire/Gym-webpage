e6765ad6c0ecf8a96b3f1e07679557b72903a919
ashagaire <asha.gaire95@gmail.com>
2024-03-06 16:37:57 +0200

    deploy render attemp 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,10 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import node from '@sveltejs/adapter-node';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: node()
   },
   preprocess: vitePreprocess()
 };
